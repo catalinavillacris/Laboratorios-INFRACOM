69a65705f324bed14f54a6321bd292b456cb754c
Lucia Castillo <d.castillop@uniandes.edu.co>
2025-02-08 19:48:35 -0500

    cleinte arreglado :p

--- laboratorio3_m.villacris_d.castillop_j.solorzano/cliente.java
+++ laboratorio3_m.villacris_d.castillop_j.solorzano/cliente.java
@@ -1,16 +1,48 @@
 import java.util.Random;
 
 public class cliente extends Thread {
+    private static int contadorClientes = 0; 
     private int uid;
-    private int numeroDeclientes = 0;
-    private fila fila;
     private int tiempoProcesamiento;
+    private fila fila;
+    private Random rand = new Random();
     
-    public cliente(int uid, int numeroDeclientes, fila fila) {
-        this.uid = uid;
-        this.numeroDeclientes = numeroDeclientes;
+    public cliente(fila fila) {
         this.fila = fila;
-        this.tiempoProcesamiento = new Random().nextInt(2001) ;
+    }
+    
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) { 
+            int uid = contadorClientes++; 
+            int tiempoProcesamiento = rand.nextInt(2001); 
+    
+            cliente nuevoCliente = new cliente(fila);
+            nuevoCliente.setUid(uid);
+            nuevoCliente.setTiempoProcesamiento(tiempoProcesamiento);
+    
+            fila.agregarCliente(nuevoCliente);
+            try {
+                Thread.sleep(rand.nextInt(501)); 
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            fila.retirarCliente();
+            try {
+                Thread.sleep(rand.nextInt(501)); 
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+    
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public void setTiempoProcesamiento(int tiempoProcesamiento) {
+        this.tiempoProcesamiento = tiempoProcesamiento;
     }
 
     public int getUid() {
@@ -20,22 +52,4 @@ public class cliente extends Thread {
     public int getTiempoProcesamiento() {
         return tiempoProcesamiento;
     }
-
-    @Override
-    public void run() {
-        fila.agregarCliente(this);
-        try {
-            Thread.sleep(new Random().nextInt(501)); 
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        fila.retirarCliente();
-        try {
-            Thread.sleep(new Random().nextInt(501)); 
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
 }
